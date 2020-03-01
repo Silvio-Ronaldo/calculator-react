005bed553c6d4fb735a296c25fea2716196fcd6d
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2020-03-01 02:17:53 -0300

    Configurações iniciais de operações

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,35 @@ import { Container, Title, Calculator, Display, Signal, Text, Keyboard, MainKeys
 
 function App() {
   const [valueDisplay, setValueDisplay] = useState('');
+  const [countNumbers, setCountNumbers] = useState(0);
+  const [total, setTotal] = useState(0);
+  let fromOperation = false;
 
-  function handleClick(i) {
-    setValueDisplay(valueDisplay + i);
+  function handleNumbers(i) {
+    if(fromOperation) {
+      setValueDisplay('');
+      fromOperation = false;
+    }
+
+    if(countNumbers < 10) {
+      setValueDisplay(valueDisplay + i);
+      setCountNumbers(countNumbers + 1);
+    } else {
+      return;
+    }   
+  }
+
+  function handleOperations(operation) {
+    if((valueDisplay !== '') && (total !== 0)) {
+      console.log(total);
+      setTotal((parseInt(valueDisplay) + total));
+      setValueDisplay(total);
+      fromOperation = true;
+    } else if(valueDisplay !== '') {
+      setTotal(parseInt(valueDisplay));
+      console.log(total);
+      fromOperation = true;
+    }
   }
 
   return (
@@ -21,32 +47,32 @@ function App() {
           <Keyboard>
             <MainKeys>
               <Line>
-                <Key onClick={ () => handleClick(7) }><Number>7</Number></Key>
-                <Key onClick={ () => handleClick(8) }><Number>8</Number></Key>
-                <Key onClick={ () => handleClick(9) }><Number>9</Number></Key>
+                <Key onClick={ () => handleNumbers(7) }><Number>7</Number></Key>
+                <Key onClick={ () => handleNumbers(8) }><Number>8</Number></Key>
+                <Key onClick={ () => handleNumbers(9) }><Number>9</Number></Key>
               </Line>
               <Line>
-                <Key onClick={ () => handleClick(4) }><Number>4</Number></Key>
-                <Key onClick={ () => handleClick(5) }><Number>5</Number></Key>
-                <Key onClick={ () => handleClick(6) }><Number>6</Number></Key>
+                <Key onClick={ () => handleNumbers(4) }><Number>4</Number></Key>
+                <Key onClick={ () => handleNumbers(5) }><Number>5</Number></Key>
+                <Key onClick={ () => handleNumbers(6) }><Number>6</Number></Key>
               </Line>
               <Line>
-                <Key onClick={() => handleClick(1) }><Number>1</Number></Key>
-                <Key onClick={() => handleClick(2) }><Number>2</Number></Key>
-                <Key onClick={() => handleClick(3) }><Number>3</Number></Key>
+                <Key onClick={() => handleNumbers(1) }><Number>1</Number></Key>
+                <Key onClick={() => handleNumbers(2) }><Number>2</Number></Key>
+                <Key onClick={() => handleNumbers(3) }><Number>3</Number></Key>
               </Line>
               <Line>
-                <Key onClick={() => handleClick(0) }><Number>0</Number></Key>
+                <Key onClick={() => handleNumbers(0) }><Number>0</Number></Key>
                 <Key><Number>.</Number></Key>
                 <Key><Number>C</Number></Key>
               </Line>  
             </MainKeys>
 
             <OperationKeys>
-                <SignalKey><Number>/</Number></SignalKey>
-                <SignalKey><Number>*</Number></SignalKey>
-                <SignalKey><Number>-</Number></SignalKey>
-                <SignalKey><Number>+</Number></SignalKey>
+                <SignalKey onClick={ () => handleOperations(4) }><Number>/</Number></SignalKey>
+                <SignalKey onClick={ () => handleOperations(3) }><Number>*</Number></SignalKey>
+                <SignalKey onClick={ () => handleOperations(2) }><Number>-</Number></SignalKey>
+                <SignalKey onClick={ () => handleOperations(1) }><Number>+</Number></SignalKey>
                 <SignalKey><Number>=</Number></SignalKey>
             </OperationKeys>
           </Keyboard>
