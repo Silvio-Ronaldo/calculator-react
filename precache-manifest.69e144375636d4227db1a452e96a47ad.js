0c3e92b16a111ae5785a897a53a0d80c1d1b1d76
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2020-04-11 20:03:17 -0300

    Updates

---
+++ precache-manifest.69e144375636d4227db1a452e96a47ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dedd24855bc2de7c8ed4894dba8fa389",
+    "url": "/calculator-react/index.html"
+  },
+  {
+    "revision": "7dcd404c73347a3b3a20",
+    "url": "/calculator-react/static/js/2.c0329b58.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/calculator-react/static/js/2.c0329b58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71f19014c912ba39a267",
+    "url": "/calculator-react/static/js/main.5ba5f107.chunk.js"
+  },
+  {
+    "revision": "fa7c83c5d349a8c9cb0f",
+    "url": "/calculator-react/static/js/runtime-main.4ba264bb.js"
+  }
+]);
